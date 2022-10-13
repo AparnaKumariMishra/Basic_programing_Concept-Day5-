3d80dbd57ba92a48b4b0652ed69c3f6f68e884aa
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-13 20:09:50 +0530

    Add files via upload
    Added Uc7

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace UserCase7SwapeNo
+{
+    internal class program
+    {
+        public static void Main()
+        {
+            UC7SwapeNo.swape();
+        }
+    }
+}
+
---
+++ UC7SwapeNo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCase7SwapeNo
+{
+    internal class UC7SwapeNo
+    {
+        public static void swape()
+        {
+            int a, b;
+            Console.WriteLine("Please enter your first no: ");
+            a = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your second no: ");
+            b = int.Parse(Console.ReadLine());
+            Console.WriteLine("Before swap a= " + a + " b= " + b);
+            a = a * b;      
+            b = a / b;    
+            a = a / b;   
+            Console.Write("After swap a= " + a + " b= " + b);
+        }
+    }
+}
