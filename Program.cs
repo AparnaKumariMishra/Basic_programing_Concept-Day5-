8fbb97a98513ba1e92012d4359e904396620bf64
AparnaKumariMishra <113542054+AparnaKumariMishra@users.noreply.github.com>
2022-10-13 20:01:59 +0530

    Add files via upload
    Added UC3

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace UserCase3PowerOf2
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            UC3Powerof2.power();
+        }
+    }
+}
+
+
---
+++ UC3Powerof2.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCase3PowerOf2
+{
+    internal class UC3Powerof2
+    {
+        public static void power()
+        {
+            int i, n;
+
+            Console.Write("\n\n");
+            Console.Write("Display the multiplication table:\n");
+            Console.Write("-----------------------------------");
+            Console.Write("\n\n");
+
+            Console.Write("Input the number for which you want to print the table : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.Write("\n");
+            for (i = 1; i <= n; i++)
+            {
+                Console.Write("{0} X {1} = {2} \n", n, i, n * i);
+            }
+        }
+    }
+}
